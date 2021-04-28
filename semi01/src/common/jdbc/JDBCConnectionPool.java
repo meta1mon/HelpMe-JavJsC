718f3e96e0d4ab750e38aa8327b6c88b42bc81cb
hdk <odgnuyk1225@gmail.com>
2021-04-28 14:21:52 +0900

    세미프로젝트 초기 코드 업로드 (쇼핑몰)

---
+++ semi01/src/common/jdbc/JDBCConnectionPool.java
@@ -0,0 +1,49 @@
+package common.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource; //javax가 관리하는 datasource
+
+public class JDBCConnectionPool {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			// 현재 환경의 naming context 획득
+			Context i1 = new InitialContext(); // 클래스로 인스턴스 생성했기때문에 클래스로 new. List<String> l1 = new
+												// ArrayList<String>();과 같은 느낌~
+			Context i2 = (Context) i1.lookup("java:/comp/env"); // web.xml 찾아가는 부분 (암기필요) / 리턴값 object 형태 -> Context 형태로
+																// 형변환
+			// DataSource 찾기
+			DataSource ds = (DataSource) i2.lookup("jdbc/myoracle");
+			
+			// 커넥션 얻어오기
+			conn = ds.getConnection();
+
+			if (conn == null) {
+				System.out.println("************ 연결 실패 *************");
+			} else {
+				System.out.println("!!!!!!!!!!!! 연결 성공 !!!!!!!!!!!!!");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		return conn;
+	}
+
+	public static void close(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
